201a2d40c8ecf5c954e9f95935109293054515e1
karlokovac <kovac.karlo5@gmail.com>
2021-07-11 23:48:58 +0200

    Further improvements

--- kotlin_intro/src/main/kotlin/com/infinum/library/Library.kt
+++ kotlin_intro/src/main/kotlin/com/infinum/library/Library.kt
@@ -3,7 +3,7 @@ package com.infinum.library
 import java.time.LocalDate
 
 object Library {
-    private val rentedBooks: MutableMap<RentedBook,String> = hashMapOf()
+    private val rentedBooks: MutableMap<Book, RentData> = hashMapOf()
     private val storedBooks: MutableList<Book> = mutableListOf(
         Book("The Hunger Games"," Suzanne Collins"),
         Book("Harry Potter and the Order of the Phoenix"," J.K. Rowling"),
@@ -26,26 +26,36 @@ object Library {
             .firstOrNull { book -> book.title==title && book.authorName==authorName }
             ?.also { book ->
                 storedBooks.remove(book)
-                rentedBooks[RentedBook(book, dueDate(duration))] = customerOIB
+                rentedBooks[book] = RentData(customerOIB, dueDate(duration) )
             }
     }
     fun returnBook(book:Book){
-        rentedBooks.keys.firstOrNull { rentedBook -> rentedBook.book == book}
+        rentedBooks.keys.firstOrNull { rentedBook -> rentedBook == book}
             ?.also { rentedBook ->
                 rentedBooks.remove(rentedBook)
-                storedBooks.add(rentedBook.book)
+                storedBooks.add(rentedBook)
             } ?: throw BookNotRentedException(book)
     }
     fun isBookRented(book: Book): Boolean{
-        return rentedBooks.keys.any{ rentedBook -> rentedBook.book == book }
+        return rentedBooks.containsKey(book)
     }
     fun getRentedBooks(customerOIB: String): List<RentedBook>{
-        return rentedBooks.keys.filter { rentedBook ->
-            rentedBooks[rentedBook] == customerOIB }
+        return rentedBooks.keys
+            .filter { book -> rentedBooks[book]!!.customerOIB == customerOIB }
+            .map { book -> RentedBook(book, rentedBooks[book]!!.dueDate ) }
     }
 
     private fun dueDate(duration: RentDuration): LocalDate = LocalDate.now().plus(duration.period)
-    data class RentedBook( val book: Book, val dueDate: LocalDate )
+
+    data class RentedBook(
+        val book: Book,
+        val dueDate: LocalDate
+    )
+
+    data class RentData(
+        val customerOIB: String,
+        val dueDate: LocalDate
+    )
 }
 
 class BookNotRentedException(book: Book) : RuntimeException("$book was never rented")
