5df9da975feda933fc9a4f3fb1e46b92b0458934
karlokovac <kovac.karlo5@gmail.com>
2021-07-14 21:02:13 +0200

    Version change

--- spring_core/build.gradle.kts
+++ spring_core/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.20"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.5.10"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.5.20"
     jacoco
 }
 
