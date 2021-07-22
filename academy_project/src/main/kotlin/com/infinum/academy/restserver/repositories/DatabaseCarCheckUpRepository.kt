b51146fef8d95ad5d54c2a516b4fa5b0bd664976
karlokovac <kovac.karlo5@gmail.com>
2021-07-22 22:39:18 +0200

    SimpleJdbcInsert swap

--- academy_project/src/main/kotlin/com/infinum/academy/restserver/repositories/DatabaseCarCheckUpRepository.kt
+++ academy_project/src/main/kotlin/com/infinum/academy/restserver/repositories/DatabaseCarCheckUpRepository.kt
@@ -1,44 +1,38 @@
 package com.infinum.academy.restserver.repositories
 
 import com.infinum.academy.restserver.models.CarCheckUp
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpStatus
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert
 import org.springframework.jdbc.support.GeneratedKeyHolder
 import org.springframework.stereotype.Repository
 import org.springframework.web.server.ResponseStatusException
 import java.sql.Timestamp
+import javax.sql.DataSource
 
 @Repository
 class DatabaseCarCheckUpRepository(
-    private val jdbcTemplate: JdbcTemplate
+    private val jdbcTemplate: JdbcTemplate,
+    dataSource: DataSource
 ) : CarCheckUpRepository {
-    companion object {
-        private const val CARCHECKUP_INSERT_SQL =
-            "INSERT INTO carCheckUps (id,workername,price,carid,datetime) VALUES (DEFAULT,?,?,?,?)"
-        private const val workerName_index = 1
-        private const val price_index = 2
-        private const val carid_index = 3
-        private const val dateTime_index = 4
-    }
+    private val simpleJdbcInsert = SimpleJdbcInsert( dataSource)
+        .withTableName("carcheckups")
+        .usingGeneratedKeyColumns("id")
 
     override fun save(carCheckUp: CarCheckUp): Long {
         return try {
-            val keyHolder = GeneratedKeyHolder()
-            jdbcTemplate.update(
-                {
-                    it.prepareStatement(CARCHECKUP_INSERT_SQL, arrayOf("id")).apply {
-                        setString(workerName_index, carCheckUp.workerName)
-                        setDouble(price_index, carCheckUp.price)
-                        setLong(carid_index, carCheckUp.carId)
-                        setTimestamp(dateTime_index, Timestamp.valueOf(carCheckUp.dateTime))
-                    }
-                },
-                keyHolder
-            )
-            keyHolder.key as Long? ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST)
-        } catch (ex: DataIntegrityViolationException) {
+            simpleJdbcInsert.executeAndReturnKey(
+                mapOf(
+                    "workername" to carCheckUp.workerName,
+                    "price" to carCheckUp.price,
+                    "carid" to carCheckUp.carId,
+                    "datetime" to carCheckUp.dateTime
+                )
+            ).toLong()
+        }catch (ex: DataIntegrityViolationException) {
             println(ex.message)
             throw ResponseStatusException(HttpStatus.NOT_FOUND)
         }
@@ -46,7 +40,7 @@ class DatabaseCarCheckUpRepository(
 
     override fun findAllByCarId(id: Long): List<CarCheckUp> {
         return try {
-            jdbcTemplate.queryForList("SELECT * FROM carcheckups WHERE carid = ?", id)
+            jdbcTemplate.queryForList("SELECT * FROM carcheckups WHERE carid = ? ORDER BY datetime DESC", id)
                 .map { row ->
                     CarCheckUp(
                         row["id"] as Long,
@@ -55,7 +49,7 @@ class DatabaseCarCheckUpRepository(
                         row["carid"] as Long,
                         (row["datetime"] as Timestamp).toLocalDateTime()
                     )
-                }.sortedByDescending { carCheckUp -> carCheckUp.dateTime }
+                }
         } catch (ex: EmptyResultDataAccessException) {
             println(ex.message)
             emptyList()
--- academy_project/src/main/kotlin/com/infinum/academy/restserver/repositories/DatabaseCarRepository.kt
+++ academy_project/src/main/kotlin/com/infinum/academy/restserver/repositories/DatabaseCarRepository.kt
@@ -1,46 +1,36 @@
 package com.infinum.academy.restserver.repositories
 
 import com.infinum.academy.restserver.models.Car
+import org.springframework.dao.DataIntegrityViolationException
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpStatus
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.queryForObject
-import org.springframework.jdbc.support.GeneratedKeyHolder
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert
 import org.springframework.stereotype.Repository
 import org.springframework.web.server.ResponseStatusException
-import java.sql.Date
+import javax.sql.DataSource
 
 @Repository
 class DatabaseCarRepository(
     private val jdbcTemplate: JdbcTemplate,
+    dataSource: DataSource
 ) : CarRepository {
-    companion object {
-        private const val CAR_INSERT_SQL = "INSERT INTO cars (id,ownerId,dateAdded,manufacturerName,modelName," +
-            "productionYear,serialNumber) VALUES (DEFAULT,?,?,?,?,?,?)"
-        private const val ownerid_index = 1
-        private const val dateAdded_index = 2
-        private const val manufacturerName_index = 3
-        private const val modelName_index = 4
-        private const val productionYear_index = 5
-        private const val serialNumber_index = 6
-    }
+    private val simpleJdbcInsert = SimpleJdbcInsert( dataSource)
+        .withTableName("cars")
+        .usingGeneratedKeyColumns("id")
 
     override fun save(car: Car): Long {
-        val keyHolder = GeneratedKeyHolder()
-        jdbcTemplate.update(
-            {
-                it.prepareStatement(CAR_INSERT_SQL, arrayOf("id")).apply {
-                    setLong(ownerid_index, car.ownerId)
-                    setDate(dateAdded_index, Date.valueOf(car.dateAdded))
-                    setString(manufacturerName_index, car.manufacturerName)
-                    setString(modelName_index, car.modelName)
-                    setInt(productionYear_index, car.productionYear)
-                    setLong(serialNumber_index, car.serialNumber)
-                }
-            },
-            keyHolder
-        )
-        return keyHolder.key as Long? ?: throw ResponseStatusException(HttpStatus.BAD_REQUEST)
+        return simpleJdbcInsert.executeAndReturnKey(
+            mapOf(
+                "ownerid" to car.ownerId,
+                "dateadded" to car.dateAdded,
+                "manufacturername" to car.manufacturerName,
+                "modelname" to car.modelName,
+                "productionyear" to car.productionYear,
+                "serialnumber" to car.serialNumber
+            )
+        ).toLong()
     }
 
     override fun findById(id: Long): Car {
